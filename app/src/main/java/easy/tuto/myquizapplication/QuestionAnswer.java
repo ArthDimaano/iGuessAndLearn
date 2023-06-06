f188f7b734d5e6ae28ff06b2425058e335b68caa
ArthDimaano <131332012+ArthDimaano@users.noreply.github.com>
2023-06-06 23:03:36 +0800

    LALOVES

--- app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
+++ app/src/main/java/easy/tuto/myquizapplication/QuestionAnswer.java
@@ -3,24 +3,27 @@ package easy.tuto.myquizapplication;
 public class QuestionAnswer {
 
     public static String question[] ={
-            "Which company owns the android?",
-            "Which one is not the programming language?",
-            "Where you are watching this video?",
-            "Which company owns the Apple?"
+            "Java - is an object-oriented programming language that produces software for multiple platforms.",
+            "Printer - a device that accept text and and graphic output from a computer and transfers the information to paper.",
+            "Virus - is a type of malicious software, or malware, that spreads between computers and causes damage to data and software.",
+            "Mouse - is a small, movable device that lets you control a range of things on a computer.",
+            "Block – a set of statements enclosed in set braces { }."
     };
 
     public static String choices[][] = {
-            {"Google","Apple","Nokia","Samsung"},
-            {"Java","Kotlin","Notepad","Python"},
-            {"Facebook","Whatsapp","Instagram","Youtube"},
-            {"Google","Apple","Nokia","Samsung"}
+            {"jaa-vuh","juh-vuh","jaa-v","juh-bah"},
+            {"phri-tur","prin-tee","prin-tr","phri-tir"},
+            {"vai-ruhs","va-rus","vay-rush","vai-rs"},
+            {"mohws","mahws","maws","mows"},
+            {"bla-ak","blaa-ak","bal-ack","bl-ach"}
     };
 
     public static String correctAnswers[] = {
-            "Google",
-            "Notepad",
-            "Youtube",
-            "Apple"
+            "jaa-vuh",
+            "prin-tr",
+            "vai-ruhs ",
+            "mows",
+            "bla-ak"
     };
 
 }
